8a9809392de112480a0f0f803b5f3ac924753de8
root <root@localhost.localdomain>
2018-07-02 20:32:02 +0530

    New checkin3 version

---
+++ rename_file3.py
@@ -0,0 +1,17 @@
+#!/export/anaconda3/bin/python
+
+import os
+import glob
+
+os.chdir("/export")
+# My test sankar
+for file1 in glob.glob("*.json"):
+    file_name = os.path.splitext(file1)[0]
+    extension = os.path.splitext(file1)[1]
+    new_file_name = file_name[:-6] + extension
+    try:
+        os.rename(file1, new_file_name)
+    except OSError as e:
+        print(e)
+    else:
+        print("Renamed {} to {}".format(file1, new_file_name))
